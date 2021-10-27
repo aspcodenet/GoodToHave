85449ed89d56793aebed6a26c94ada5ce0593c95
Stefan Holmberg <stefan.holmberg@systementor.se>
2021-10-27 14:08:39 +0200

    first commit

---
+++ functions.py
@@ -0,0 +1,7 @@
+def getInputBetween(startval: int,endval: int)->int: 
+    while True: 
+        try: 
+            val=int(input("Mata in:")) 
+            if val >= startval and val <= endval: 
+                return val print(f"Ogiltigt val, mellan {startval} och {endval}, tack") 
+            except: print("Ange ett tal tack!")
---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='goodtohave',
+    version='0.0.1',
+    author='Stefan Holmberg',
+    author_email='stefan.holmberg@systementor.se',
+    description='Good to have things',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/aspcodenet/goodtohave',
+    project_urls = {
+        "Bug Tracker": "https://github.com/aspcodenet/goodtohave/issues"
+    },
+    license='MIT',
+    packages=['goodtohave'],
+    install_requires=['requests'],
